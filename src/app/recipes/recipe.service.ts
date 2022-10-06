8dde873e92a7cd1280b201f0bc5474a35fd6e8c7
Arvin Vahidkia <arvinvahidkia1@gmail.com>
2022-10-06 18:58:42 +0330

    Routing Added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,7 +7,6 @@ import { Recipe } from './recipe.model';
   providedIn: 'root',
 })
 export class RecipeService {
-
   constructor(private shoppingService: ShoppingListService) {}
 
   recipeSelected = new EventEmitter<Recipe>();
@@ -17,25 +16,22 @@ export class RecipeService {
       'A Test Recipe',
       'This is simply a test',
       'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'Another Test Recipe',
       'This is simply a test',
       'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ]
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
 
-
-  getRecipe() {
+  getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
   addIngredientToShoppingList(ingredients: Ingredient[]) {
